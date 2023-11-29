c326178f5e81458205a4b2b6e46b319780d96201
Jonas Jensen <jbj@github.com>
2023-11-29 15:31:15 +0100

    Reapply "Initial fixups"
    This reverts commit e2423cc131790863e2433f0b98ad7d4b620e1e64.

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,118 @@
+import * as cache from '@actions/cache'
 import * as core from '@actions/core'
-import { wait } from './wait'
+import * as fs from 'fs'
+import * as github from '@actions/github'
 
-/**
- * The main function for the action.
- * @returns {Promise<void>} Resolves when the action is complete.
- */
+async function shouldSkip(): Promise<boolean> {
+  const commit = process.env['GITHUB_SHA']
+  const branch = process.env['GITHUB_REF']
+  const workflow = process.env['GITHUB_WORKFLOW']
+  if (!commit) {
+    core.error('GITHUB_SHA is not defined')
+    return false
+  }
+  if (!branch) {
+    core.error('GITHUB_REF is not defined')
+    return false
+  }
+  if (!workflow) {
+    core.error('GITHUB_WORKFLOW is not defined')
+    return false
+  }
+
+  const cachePaths: string[] = ['filelist.txt']
+  const primaryKey = `${workflow}-${branch}-${commit}`
+  const restoreKeys: string[] = [`${workflow}-${branch}-`]
+
+  const cacheKey = await cache.restoreCache(
+    cachePaths,
+    primaryKey,
+    restoreKeys,
+    { lookupOnly: false },
+    false
+  )
+  if (!cacheKey) {
+    core.info(
+      `Cache not found for input keys: ${[primaryKey, ...restoreKeys].join(
+        ', '
+      )}`
+    )
+    return false
+  }
+  const previousCommit = cacheKey.split('-').pop()
+  if (!previousCommit) {
+    core.warning(`Malformed cache key: ${cacheKey}`)
+    return false
+  }
+
+  const changedFiles = await getChangedFiles(previousCommit, commit)
+  const usedFiles = new Set(
+    fs.readFileSync('filelist.txt').toString().split('\n')
+  )
+  for (const file of changedFiles) {
+    if (usedFiles.has(file)) {
+      return false
+    }
+  }
+  return true
+}
+
 export async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
+    const skip = await shouldSkip()
+    core.setOutput('skip', skip.toString())
+    if (skip) {
+      core.info('Skipping workflow run')
+    } else {
+      core.info('Running workflow')
+      //TODO: setup tracing
+    }
+  } catch (error) {
+    // Fail the workflow run if an error occurs
+    if (error instanceof Error) core.setFailed(error.message)
+  }
+}
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
+async function getChangedFiles(
+  base: string,
+  head: string
+): Promise<Set<string>> {
+  // use github rest api to get changed files
+  // https://docs.github.com/en/rest/commits/commits#compare-two-commits
+  const token = process.env['GITHUB_TOKEN'] || ''
+  const octokit = github.getOctokit(token)
+  const [owner, repo] = process.env['GITHUB_REPOSITORY']?.split('/', 2) || []
+  const { data: data } = await octokit.rest.repos.compareCommits({
+    owner,
+    repo,
+    base,
+    head
+  })
+  const changedFiles = new Set<string>()
+  if (data.files) {
+    for (const file of data.files) {
+      changedFiles.add(file.filename)
+    }
+  }
+  return changedFiles
+}
 
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+export async function finish(): Promise<void> {
+  try {
+    const commit = process.env['GITHUB_SHA']
+    const branch = process.env['GITHUB_REF']
+    const workflow = process.env['GITHUB_WORKFLOW']
 
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+    const cachePaths: string[] = ['filelist.txt']
+    const primaryKey = `${workflow}-${branch}-${commit}`
+
+    //TODO: end tracing and collect paths in filelist.txt
+
+    const cacheId = await cache.saveCache(cachePaths, primaryKey, {}, false)
+
+    if (cacheId !== -1) {
+      core.info(`Cache saved with key: ${primaryKey}`)
+    }
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
