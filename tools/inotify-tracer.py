14fa584ef6f23fd1a5665c501c3349a92315ecae
Jonas Jensen <jbj@github.com>
2023-11-30 22:04:17 +0100

    Run python-black to reformat

--- tools/inotify-tracer.py
+++ tools/inotify-tracer.py
@@ -5,26 +5,28 @@ from inotify_simple import INotify, flags
 
 watch_flags = flags.ACCESS | flags.MODIFY | flags.ATTRIB | flags.OPEN
 
+
 def debug(*args, **kwargs):
     print("inotify tracer:", *args, file=sys.stderr, **kwargs)
 
+
 def main():
     root = sys.argv[1]
-    
+
     # We need to construct this list up front since adding new watches causes
     # events to be generated, and we don't want to see our own events.
     dirs_to_watch = []
-    
+
     inotify = INotify()
-    
+
     debug("Enumerating directories under", root)
     # Watch all directories under root, recursively.
     # We shouldn't need to add newly directories to the watch list dynamically
     # since we're just interested in what was in the repo at checkout time.
     for path, dirs, files in os.walk(root):
         # Ignore .git directories recursively by removing them from the list
-        if '.git' in dirs:
-            dirs.remove('.git')
+        if ".git" in dirs:
+            dirs.remove(".git")
         dirs_to_watch.append(path)
     debug("Found", len(dirs_to_watch), "directories")
 
@@ -40,7 +42,7 @@ def main():
 
     while True:
         for event in inotify.read():
-            if event.mask & flags.ISDIR or event.name == '':
+            if event.mask & flags.ISDIR or event.name == "":
                 # We don't care about directory events.
                 continue
             fullname = str(os.path.join(dir_names[event.wd], event.name))
@@ -50,5 +52,6 @@ def main():
             files_seen.add(fullname)
             print(fullname, flush=True)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
