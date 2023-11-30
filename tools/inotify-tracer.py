8f65714ae79d122e17e184e8530313e2cd97e9e4
Jonas Jensen <jbj@github.com>
2023-11-30 21:53:37 +0100

    Print debug output to stderr and prefix tool name

--- tools/inotify-tracer.py
+++ tools/inotify-tracer.py
@@ -5,6 +5,9 @@ from inotify_simple import INotify, flags
 
 watch_flags = flags.ACCESS | flags.MODIFY | flags.ATTRIB | flags.OPEN
 
+def debug(*args, **kwargs):
+    print("inotify tracer:", *args, file=sys.stderr, **kwargs)
+
 def main():
     root = sys.argv[1]
     
@@ -14,7 +17,7 @@ def main():
     
     inotify = INotify()
     
-    print("Enumerating directories under", root)
+    debug("Enumerating directories under", root)
     # Watch all directories under root, recursively.
     # We shouldn't need to add newly directories to the watch list dynamically
     # since we're just interested in what was in the repo at checkout time.
@@ -23,16 +26,15 @@ def main():
         if '.git' in dirs:
             dirs.remove('.git')
         dirs_to_watch.append(path)
-    print("Found", len(dirs_to_watch), "directories")
+    debug("Found", len(dirs_to_watch), "directories")
 
     dir_names = {}
 
-    print("Registering watches")
+    debug("Registering watches")
     for dir in dirs_to_watch:
         wd = inotify.add_watch(dir, watch_flags)
         dir_names[wd] = dir
-    print("Starting to monitor")
-    print()
+    debug("Starting to monitor\n")
 
     files_seen = set()
 
